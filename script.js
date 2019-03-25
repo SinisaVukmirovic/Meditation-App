e8295aef1b90693381013b6c3d03b3f27d1c61e2
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2019-03-25 17:55:17 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,85 @@
+const app = () => {
+    // getting the DOM elements
+    const song = document.querySelector('.song');
+    const play = document.querySelector('.play');
+    const outline = document.querySelector('.moving-outline circle');
+    const video = document.querySelector('.vid-container video');
+
+    // sounds
+    const sounds = document.querySelectorAll('.sound-picker button');
+
+    // time display
+    const timeDisplay = document.querySelector('.time-display');
+    const timeSelect = document.querySelectorAll('.time-select button');
+
+    // getting the lenght of the outline
+    const outlineLength = outline.getTotalLength();
+    
+    // chosen time duration
+    let duration = 120;
+
+    outline.style.strokeDasharray = outlineLength;
+    outline.style.strokeDashoffset = outlineLength;
+
+    // picking sounds
+    sounds.forEach(sound => {
+        sound.addEventListener('click', function() {
+            song.src = this.getAttribute('data-sound');
+            video.src = this.getAttribute('data-video');
+            checkPlaying(song);
+        });
+    });
+
+    // play sounds
+    play.addEventListener('click', () => {
+        checkPlaying(song);
+    });
+
+    // select sound 
+    timeSelect.forEach(option => {
+        option.addEventListener('click', function() {
+            duration = this.getAttribute('data-time');
+            timeDisplay.textContent = `${Math.floor(duration / 60)}:0${Math.floor(duration % 60)}`;
+        }); 
+    });
+
+    // play sound and video functionality
+    const checkPlaying = () => {
+        if (song.paused) {
+            song.play();
+            video.play();
+            play.src = 'svg/pause.svg';
+        }
+        else {
+            song.pause();
+            video.pause();
+            play.src = 'svg/play.svg';
+        }
+    }
+
+    // animating the circle
+    song.ontimeupdate = () => {
+        let currentTime = song.currentTime;
+        let elapsed = duration - currentTime;
+        let seconds = Math.floor(elapsed % 60);
+        let minutes = Math.floor(elapsed / 60);
+
+        let progress = outlineLength - (currentTime / duration) * outlineLength;
+        outline.style.strokeDashoffset = progress;
+        
+        // animating the text
+        timeDisplay.textContent = `${minutes}:${seconds}`;
+
+        // reseting the song after the time runs out
+        if(currentTime >= duration) {
+            song.pause();
+            song.currentTime = 0;
+            play.src = 'svg/play.svg';
+            video.pause();
+        }
+
+    }
+    
+}
+
+app();
